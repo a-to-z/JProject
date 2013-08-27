b46c1c192c494b9f295895f303190fe278baedf9
Chachar <chachar@ubuntu.(none)>
2013-08-27 18:42:51 +0500

    Changed DB and added MetaData

--- DBCon.java
+++ DBCon.java
@@ -5,11 +5,12 @@ class DBCon
 {
 	//Class for DB Connection on MySQL
 	private String dbClassName = "com.mysql.jdbc.Driver";
-	private String CONNECTION = "jdbc:mysql://127.0.0.1/one";
+	public String CONNECTION = "jdbc:mysql://127.0.0.1/Project";
 	Connection c;
 	private Properties p;
 	private Statement st;
 	private ResultSet rs;
+	DatabaseMetaData md;
 
 	public DBCon() throws ClassNotFoundException, SQLException
 	{
@@ -20,42 +21,54 @@ class DBCon
 		p.put("password", "root");
 		c = DriverManager.getConnection(CONNECTION, p);
 		st = c.createStatement();
+		md = c.getMetaData();
 	}
-	public void getAllRecords() throws SQLException
+	public ResultSet getAllRecords(String tablename) throws SQLException
 	{
 		//Method gets all the records but does not return the result set for GUI yet.
-		String query = "SELECT * FROM trial;";
+		String query = "SELECT * FROM " + tablename + ";";
 		rs = st.executeQuery(query);
 		int rec = 0;
 		while(rs.next())
 		{
 			System.out.println("-------------------------------------------");
 			System.out.println("Record #" + (rec+1) + ":");
-			System.out.println("ID:   " + rs.getString("id") + "\n" +
-					"Name: " + rs.getString("name"));
+			System.out.println("ID:   " + rs.getString("ID") + "\n" +
+					"First Name: " + rs.getString("FNAME") + 
+					"Last Name: " + rs.getString("LNAME") +
+					"Address: " + rs.getString("ADDRESS") +
+					"Marks: " + rs.getString("MARKS") +
+					"Contact Info: " + rs.getString("CONTACT"));
 			rec++;
 		}
 		System.out.println("-------------------------------------------");
 		System.out.println("All Records Displayed Successfully!");
 		System.out.println("-------------------------------------------");
+		return rs;
 	}
 	public void closeConnection() throws SQLException
 	{
 		//Fancy way of closing the con lol
 		c.close();
 	}
-	public void setRecord(int id, String name) throws SQLException
+	public void setRecord(int id, String[] items) throws SQLException
 	{
 		//Sets Records to DB
-		String query = "INSERT INTO trial VALUES (" +id+ ", \"" +name+ "\");";
+		String query = "INSERT INTO trial VALUES (" + id;
+		for(int i = 0; i < items.length ; i++)
+		{
+			System.out.println(items[i]);
+			query += ", \"" + items[i] + "\"";
+		}
+		query += ");";
 		st.executeUpdate(query);
 	}
-	public void delRecord(int id, String name) throws SQLException
+	public void delRecord(int id, String table) throws SQLException
 	{
 		//Deletes record from DB
-		String query = "DELETE FROM trial WHERE id=" +id+ " AND name=\"" +name+ "\";";
-		boolean success = st.execute("SELECT * FROM trial WHERE id=" + 
-				id + " AND name =\"" + name + "\";");
+		String query = "DELETE FROM " + table + " WHERE id=" + id + ";";
+		boolean success = st.execute("SELECT * FROM " + table + 
+				" WHERE id=" + id+ ";");
 		if(success)
 		{
 			st.execute(query);
--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
-import java.util.*;
 import java.sql.*;
-
+import java.util.*;
 public class Main
 {
 	public static void main(String[] args)
 	{
 		GUI JProject = new GUI();
-		//Are you really expecting me to explain?
+		try
+		{
+			DBCon so = new DBCon();
+			String[] recs = { "Okay" };
+			so.setRecord(12, recs);
+			so.getAllRecords("trial");
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		catch(SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
 	}
 }
